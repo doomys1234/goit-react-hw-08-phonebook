53eba5381f1a9d8a9f122c6746df1bb27bd4c5a9
doomys1234 <doomys1234@gmail.com>
2022-05-16 00:13:20 +0300

    .

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import { store } from '../src/redux/store'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from "react-router-dom";
-// import { PersistGate } from 'redux-persist/integration/react'
 import './index.css';
 import App from './App';
 
@@ -13,11 +12,9 @@ import App from './App';
 ReactDOM.createRoot(document.getElementById('root')).render(
  <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename='https://github.com/doomys1234/goit-react-hw-08-phonebook'>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
+      <BrowserRouter basename='/https://github.com/doomys1234/goit-react-hw-08-phonebook/'>
         <App />
         </BrowserRouter>
-        {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>,
 );
