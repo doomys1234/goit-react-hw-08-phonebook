193dcc5f30599395dd00d527d112b4d4d8d663a7
doomys1234 <doomys1234@gmail.com>
2022-05-17 01:51:57 +0300

    .

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,16 +3,37 @@ import { contactsApi } from './apiSlice';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import authReducer from './auth/authSlice';
 import contactReducer from '../redux/contactSLice';
+import storage from 'redux-persist/lib/storage';
+import persistReducer from 'redux-persist/lib/persistReducer';
+import persistStore from 'redux-persist/es/persistStore';
+import { FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER, } from 'redux-persist';
+
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+}
 
 
 export const store = configureStore({
   reducer: {
     contacts: contactReducer,
     [contactsApi.reducerPath]: contactsApi.reducer,
-    auth: authReducer,
+    auth: persistReducer(authPersistConfig, authReducer),
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware().concat(contactsApi.middleware),
   
+  middleware: getDefaultMiddleware => [
+    ...getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+    contactsApi.middleware,
+  ],
 });
 setupListeners(store.dispatch);
+export const persistor = persistStore(store);
+
